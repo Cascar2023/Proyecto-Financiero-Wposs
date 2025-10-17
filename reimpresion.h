697f3fc2dc272c1c3f23318c92d737141fbc55ca
Cascar20 <johansq0708@gmail.com>
2025-10-17 13:58:02 -0500

    makefile funcional

--- reimpresion.h
+++ reimpresion.h
@@ -2,8 +2,6 @@
 #define REIMPRESION_H
 #include "compra.h"
 
-char compras[20]; // Array para almacenar las compras
-
 void reimprimir();
 void enmascararPAN(char *linea);
 void imprimirTablaHeader();
--- reimprimir.c
+++ reimprimir.c
@@ -1,6 +1,7 @@
 #include "reimpresion.h"
 #include "compra.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
